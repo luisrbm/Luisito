627f72cb962977cba2f13e60d00158aa51207f0c
Luís R. B. Machado <luisrbm@gmail.com>
2024-09-03 22:14:55 -0300

    Aprimorada lógica de inputs

--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ const words = ['balao', 'carne', 'junto', 'xinga', 'pombo', 'trico', 'doula', 'm
 function iniciar(){
 	document.getElementById('jogos');
 	const forms = [];
+	var active = 0;
 	for (let i=0;i<=5;i++){
 		forms.push(document.createElement('div'));
 		forms[i].name = 'linha' + (i+1);
@@ -10,34 +11,38 @@ function iniciar(){
 		forms[i].className = 'formContainer';
 		forms[i].contenteditable = true;
 		document.getElementById('jogos').appendChild(forms[i]);
+
 		const casinhas = [];
 		for (let j=0;j<=4;j++){
-			casinhas.push(document.createElement('div'));
-			casinhas[j].contentEditable = true;
+			casinhas.push(document.createElement('input'));
 			casinhas[j].name = (i+1).toString() + (j+1).toString();
 			casinhas[j].id = (i+1).toString() + (j+1).toString();
 			casinhas[j].className = 'letra';
-			// debugger;
-			casinhas[j].addEventListener('keyup', validarInput);
-			// debugger;
+			casinhas[j].maxLength = 1;
+			(i !== 0) ? casinhas[j].disabled = true : casinhas[j].disabled = false;
+			casinhas[j].addEventListener('input', validarInput)
 			forms[i].appendChild(casinhas[j]);
 		}
 	}
 }
 
-function validarInput(id){
-	let arrId = this.id.split('');
-	let nId = [parseInt(arrId[0]), parseInt(arrId[1])];
-	let letra = document.getElementById(this.id).value.toUpperCase();
+function validarLinha() {
+	const linhas = document.querySelectorAll(".formContainer");
+	for (let i; i<linhas.lenght();i++){
+		linhas[i].addEventListener('input', )
+	}
+}
 
-	if (letra.length === 1){
-		if (nId[1] < 5){
-			document.getElementById(nId[0].toString() + (nId[1]+1).toString()).focus();
-		}
-		else {
-			document.getElementById((nId[0]+1).toString() + '1').focus();
-		}
-    }
+function validarInput(){
+	const valor = document.getElementById(this.id);
+	valor.value = valor.value.toUpperCase();
+	if ((valor.value.length === 1) && (valor.id.split("")[1] !== '5')) {
+		valor.nextSibling.focus();
+	}
+	if ( ( (valor.value.length === 0) && (valor.id.split("")[1] !== '1') ) ) {
+		valor.previousSibling.focus();
+	}
+
 }
 
 function limpar(){
@@ -47,8 +52,4 @@ function limpar(){
 			document.getElementById(strId).value = "";
 		}
 	} 
-}
-
-function validarLinha(){
-
 }
